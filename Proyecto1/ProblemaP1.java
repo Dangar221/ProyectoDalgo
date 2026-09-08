35c2745c5b01290f15014555d6facb7506c216db
Dangar221 <di.garcia@uniandes.edu.co>
2026-09-07 23:24:05 -0300

    Add files via upload

---
+++ Proyecto1/ProblemaP1.java
@@ -0,0 +1,54 @@
+// Autores:
+//   Gabriela Campos - 
+//   Danna Garcia - 202320823
+//   Jose Manuel - 
+//
+// ISIS2112 - Diseño de Algoritmos
+// Semestre 2026-20
+// Proyecto Parte 1 - Problema P1
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemaP1 {
+
+    // Clase auxiliar para representar una arista del grafo
+    static class Arista {
+        // TODO: atributos
+    }
+
+    public static void main(String[] args) throws Exception {
+        // TODO
+    }
+
+    public void resolverCaso(BufferedReader lectorLineas) throws Exception {
+        // TODO
+    }
+
+    public int[] leerEnergiasOrbitas(BufferedReader lectorLineas, int numeroOrbitas) throws Exception {
+        // TODO
+        return null;
+    }
+
+    public int[][] leerPortales(BufferedReader lectorLineas, int numeroPortales) throws Exception {
+        // TODO
+        return null;
+    }
+
+    public List<List<Arista>> construirGrafo(int numeroOrbitas, int numeroPosiciones, int[] energiaOrbita, int[][] portales) {
+        // TODO
+        return null;
+    }
+
+    public int calcularNumeroNodo(int orbita, int posicion, int numeroPosiciones) {
+        // TODO
+        return 0;
+    }
+
+    public long dijkstra(List<List<Arista>> grafo, int nodoInicio, int nodoDestino, int totalNodos) {
+        // TODO
+        return 0;
+    }
+}
